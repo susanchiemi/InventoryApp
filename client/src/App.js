27089b6f512b0d8e9e1a2fee02204b1dcb3415d4
Susan Chiemi <Susan@Users-MacBook.local>
2017-10-03 21:11:26 -0400

    new commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
   render() {
     return (
       <div id="root">
-  <div class="container">
-  <div class="header clearfix">
+  <div className="container">
+  <div className="header clearfix">
         <nav>
-          <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="#">Home</a></li>
+          <ul className="nav nav-pills pull-right">
+            <li role="presentation" className="active"><a href="#">Home</a></li>
             <li role="presentation"><a href="#">About</a></li>
             <li role="presentation"><a href="#">Contact</a></li>
           </ul>
@@ -18,19 +18,19 @@ class App extends Component {
       </div>
       <br/>
       
-    <div class="jumbotron">
+    <div className="jumbotron">
     <h3>Login To Download Custom Report</h3>
     <br/>
        <form>
-       <div class="form-group">
+       <div className="form-group">
          <label for="emailInput">Email address</label>
-         <input type="email" class="form-control" id="emailInput" placeholder="Email"/>
+         <input type="email" className="form-control" id="emailInput" placeholder="Email"/>
        </div>
-       <div class="form-group">
+       <div className="form-group">
        <label for="passwordInput">Password</label>
-       <input type="password" class="form-control" id="passwordInput" placeholder="Password"/>
+       <input type="password" className="form-control" id="passwordInput" placeholder="Password"/>
        </div>
-      <button type="submit" class="btn btn-lg btn-warning">Submit</button>
+      <button type="submit" className="btn btn-lg btn-warning">Submit</button>
        </form>
         </div>
      </div>
